8192be62cbbd98a173c19521afc6437cf70625d7
safasuli01 <safa.abduallah2001@hotmail.com>
2024-08-24 00:42:12 +0300

    urls debug

--- itian_project/itian_project/urls.py
+++ itian_project/itian_project/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("Account/", include("account.urls")),
-    path("Tracking/", include("track.urls")),
+    path("Track/", include("track.urls")),
     path("Trainee/", include("trainee.urls")),
 ]
